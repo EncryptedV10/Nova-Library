d0199c3aea6c0fd7d2e90a290dcea303465ff974
Encrypted <marwanlecourtabram91670@gmail.com>
2025-06-22 11:52:38 +0200

    Update and rename Exemple Code to Exemple Code.lua

---
+++ Exemple Code.lua
@@ -0,0 +1,34 @@
+-- Booting
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/EncryptedV10/Nova-Library/refs/heads/main/Library.lua", true))()
+-- Window Creation
+local window = Library:AddWindow("Library Name")
+-- Tab Creation
+local tab = window:CreateTab("tab")
+
+-- Toggle Creation
+local Toggle = tab:AddToggle("Toggle", function(state)
+	print("Toggle is now:", state)
+end)
+
+-- Label Creation
+local Label = tab:AddLabel("Label")
+
+-- TextBox Creation
+local TextBox = tab:AddTextbox("TextBox", function(text)
+	print("You typed:", text)
+end)
+
+-- Button Creation
+local button = tab:AddButton("Button", function()
+	print("Button was clicked!")
+end)
+
+-- Slider Creation
+local Slider = tab:AddSlider("slider", 0, 100, function(val)
+	print("Slider value:", val)
+end)
+
+-- Dropdown Creation
+local Dropdown tab:AddDropdown("Dropdown", {"Option1", "Option2", "Option3"}, function(selected)
+	print("You selected:", selected)
+end)
